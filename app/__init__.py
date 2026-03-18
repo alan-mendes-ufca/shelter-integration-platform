3c2b8c03a1f6e68eff860640aff98634ec40f13d
Salomão <salomao.silva@aluno.ufca.edu.br>
2026-03-17 23:32:04 -0300

    feat: add models and controllers for Professional and Record

--- app/__init__.py
+++ app/__init__.py
@@ -26,10 +26,12 @@ from .controllers.blueprint_example import blueprint_example
 from .controllers.pessoa_rua import pessoas_bp
 from .controllers.consentimentos import consentimentos_bp
 from .controllers.atendimentos import atendimentos_bp
-from .controllers.prontuarios import profissionais_bp, prontuarios_bp
+from .controllers.prontuarios import prontuarios_bp
 from .controllers.abrigos import abrigos_bp, vagas_bp
 from .controllers.encaminhamentos import encaminhamentos_bp
+from .controllers.professional_controller import profissionais_bp
 from .docs.swagger import init_swagger
+
 
 API_PREFIX = "/api/v1"  # Prefixo comum para todas as rotas da API
 
